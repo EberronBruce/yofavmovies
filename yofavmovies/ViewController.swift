46cd24916ada65809e8e730c87035cb2ff6f874c
Bruce Burgess <bruceburgess67@yahoo.com>
2016-04-02 20:25:30 +0700

    Initial Commit

---
+++ yofavmovies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  yofavmovies
+//
+//  Created by Bruce Burgess on 4/2/16.
+//  Copyright © 2016 Red Raven Computing Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
